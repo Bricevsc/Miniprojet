dcca08f70094ffff5e41ef8d368048bd3f55ab21
Glennlg <glenn.legal@freyja.software>
2022-09-19 12:08:01 +0200

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import mongoose from "mongoose";
+import session from 'express-session';
+import router from "./router/index.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
+//Démarrage express
+const app = express();
+const port = 3000;
+//connection a la db
+mongoose.connect("mongodb://localhost:27017/titanic", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(express.static("public"));
+app.set("view engine", "pug");
+
+// récupérer les données post sous forme d'un JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/", router);
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
