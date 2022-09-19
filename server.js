3a85eead402550da17aa6c1c2837371dbe0d5a01
Glennlg <glenn.legal@freyja.software>
2022-09-19 17:34:24 +0200

    login/register/usersDB

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-// import session from 'express-session';
+import session from 'express-session';
 import router from "./router/index.js";
 import * as dotenv from 'dotenv'
 dotenv.config()
@@ -19,6 +19,23 @@ app.set("view engine", "pug");
 // récupérer les données post sous forme d'un JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(
+    session({
+        name: "login",
+        secret: "alklkdin987hdjd",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.use(function (req, res, next) {
+    app.locals = {
+        title: "Login",
+        isAuth: req.session?.auth ? req.session.auth : false
+    };
+    next();
+});
 
 app.use("/", router);
 
