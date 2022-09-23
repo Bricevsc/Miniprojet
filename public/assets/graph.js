0295c6b38f9b82448c9626aeadbf4de1d8f84137
brice Marcais <brice.marcais@enia.green>
2022-09-23 10:29:49 +0200

    graph update

--- public/assets/graph.js
+++ public/assets/graph.js
@@ -1,29 +1,23 @@
-// import  from './dashboard.js'
-
-const ctx = document.getElementById('myChart').getContext('2d');
+const ctx = document.getElementById('myChart').getContext('2d')
 const myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Total passagers','Recherche'],
-        datasets: [{
-            label: '',
-            data: [891,],
-            backgroundColor: [
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderColor: [
-                'rgba(54, 162, 235, 1)',
-                'rgba(54, 162, 235, 0.2)',
-            ],
-            borderWidth: 1
-        }]
+  type: 'doughnut',
+  data: {
+    labels: ['Total passagers', 'femme 30 1class'],
+    datasets: [
+      {
+        label: 'Total passagers',
+        data: [891, 423],
+        backgroundColor: ['rgba(54, 162, 235, 0.2)', 'rgba(255, 0, 0, 0.2)'],
+        borderColor: ['rgba(54, 162, 235, 1)', 'rgba(255, 0, 0, 1)'],
+        borderWidth: 0.5,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
     },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
+  },
+})
